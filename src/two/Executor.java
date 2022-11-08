86244df02c00f7b85e49a15191a2b441b3e3891e
ivanavukojevicokanovic <ivana.vukojevic.tz.96@gmail.com>
2022-11-08 13:05:55 +0100

    Executor with main function

--- src/two/Executor.java
+++ src/two/Executor.java
@@ -1,7 +1,24 @@
 package two;
-
+//
 public class Executor {
     public static void main(String[] args) {
+//
+        byte prviBroj = 35;
+        short drugiBroj = -32768;
+        int treciBroj = 567443323;
+        long cetvrtiBroj = 5436547678l;
+        long suma = prviBroj + drugiBroj + treciBroj + cetvrtiBroj;
+        System.out.println(suma);
+        float prviBrojDecimalni = 23.4f;
+        double drugiBrojDecimalni = 23.4;
+        System.out.println(prviBrojDecimalni + drugiBroj);
+        System.out.println(drugiBrojDecimalni);
+        char slovo = 65;
+        System.out.println(slovo + prviBroj);
+        System.out.println(slovo);
+        boolean condition = treciBroj==drugiBrojDecimalni;
+        System.out.println(condition);
+
 
     }
 }
---
+++ src/two/UnaryDemo.java
@@ -0,0 +1,10 @@
+package two;
+
+public class UnaryDemo {
+    public static void main(String[] args) {
+        int number = -23;
+        number = - number;
+        System.out.println(number);
+
+    }
+}
